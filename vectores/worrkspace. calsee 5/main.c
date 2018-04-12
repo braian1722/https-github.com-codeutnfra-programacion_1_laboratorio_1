c8069493169cc7462e76fc08e6ea9cd596a8fb9a
braia1722 <braian_1721@hotmail.com>
2018-04-12 12:16:36 -0300

    Signed-off-by: braia1722 <braian_1721@hotmail.com>

--- vectores/worrkspace. calsee 5/main.c
+++ vectores/worrkspace. calsee 5/main.c
@@ -4,11 +4,17 @@
 
 
 void mostrarvector(int [],int);
+void modificarvalor(int[]);
+int mostrarmaximo (int[],int);
+
+
 int main()
 {
 
     int vector [TAM]; //ocupa 20 bites   iniciar un vecto en 0 automaticamente todos son 0
     int i;
+    int edad;
+    int mayor;
 
 
      for (i=0; i<TAM;i++)
@@ -28,27 +34,49 @@ int main()
     /*mostrar vector*/
 
 
-
-
-
-
-
-
-
-
 mostrarvector(vector, TAM);
+
+  /*modificarvalor(vector);  */
+
+mostrarmaximo( vector,TAM);
+printf("\n el maximo es %d",mayor);
+
     return 0;
 }
 
 void mostrarvector(int vec [],int tam)
 {
     int i;
+
      for (i=0; i<tam;i++)                   //mostrar cualquier vetor de tipo entero.
     {
         printf("%d--  %d\n",i, vec[i]);
     }
 }
 
+/*void modificarvalor(int vec[])
+{
+    vec [3] =789;
+}
+*/
+
+int mostrarmaximo (int vec[],int tam)
+{
+    int maximo;
+    int i;
+    int flag = 0;
+    for (i=0;i<tam;i++)
+    {
+        if (flag ==0 || vec[i >maximo])
+        {
+            maximo =vec[i];
+            flag=1;
+
+        }
+    }
+
+  return maximo;
+  }
 
 
 
